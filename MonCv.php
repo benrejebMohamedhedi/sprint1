d9fc6c849649d9ec42bad7d1da17e09445281125
BEN REJEB Mohamed hedi <mohamedhedi.benrejeb@esprit.tn>
2018-07-27 16:47:02 +0100

    Debut du sprint 2

---
+++ MonCv.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html>
+	<head>
+		<title> MON CV </title>
+		
+		<link rel="stylesheet" type="text/css" href="stylecv.css"/>
+	</head>
+	
+	<body>
+		<div class="container">	
+			<div class="navbar">
+				<ul>
+				  <li><a class="active" href="#">Mon compte</a></li>
+				  <li><a href="cvAcreer.php">Créer mon CV</a></li>
+				  <li><a href="#">Modifier mon CV</a></li>
+				  <li style="float:right"><a href="logout.php">Logout</a></li>
+				</ul>
+			</div>	
+			
+			<h2>
+				Bienvenue sur votre compte </br>
+				Créer votre CV en ligne	
+			</h2>
+		</div>
+	</body>
+</html>
